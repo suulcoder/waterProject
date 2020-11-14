ee8da75483ef3148e2593a3ab5dbb2202ad78bfd
DianaXimena <dianaxime0@gmail.com>
2020-11-14 16:26:11 -0600

    switch sobre ecofiltro with agua contaminada y tu salud

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,12 +1,12 @@
 import { Actions } from 'react-native-router-flux';
 import { connect } from 'react-redux';
 import { normalize } from '../../utils/normalize';
-import {Text, View, Image, TouchableOpacity } from 'react-native';
-import {URL} from '../../../configuration'
+import { Text, View, Image, TouchableOpacity } from 'react-native';
+import { URL } from '../../../configuration'
 import React from 'react';
 import styles from './styles'
 
-const Menu = ({about, contact, info}) => (
+const Menu = ({ about, contact, info }) => (
     <View style={styles.container} >
         <Text style={styles.welcomeText}>
             {'¡Bienvenido!'}
@@ -16,40 +16,41 @@ const Menu = ({about, contact, info}) => (
                 <TouchableOpacity style={styles.element}>
                     <Image style={{
                         alignSelf: 'center',
-                        height:normalize(100),
-                        width:normalize(100),
+                        height: normalize(100),
+                        width: normalize(100),
                     }} source={require('../../public/icons/test.png')} ></Image>
-                </TouchableOpacity>    
+                </TouchableOpacity>
                 <Text style={styles.text}> {'Evalúa el agua'} </Text>
             </View>
-            <View>
-                <TouchableOpacity style={styles.element} onPress={about}>
-                    <Image style={{
-                        alignSelf: 'center',
-                        height:normalize(120),
-                        width:normalize(70),
-                    }} source={require('../../public/icons/about.png')} ></Image>
-                </TouchableOpacity>
-                <Text style={styles.text}> {'Sobre Ecofiltro'} </Text>
-            </View>
-        </View>
-        <View style={styles.row}>
             <View>
                 <TouchableOpacity style={styles.element} onPress={info}>
                     <Image style={{
                         alignSelf: 'center',
-                        height:normalize(100),
-                        width:normalize(100),}}
-                    source={require('../../public/icons/water.png')} ></Image>
+                        height: normalize(100),
+                        width: normalize(100),
+                    }}
+                        source={require('../../public/icons/water.png')} ></Image>
                 </TouchableOpacity>
                 <Text style={styles.text}> {'Agua contaminada y tu salud'} </Text>
+            </View>
+        </View>
+        <View style={styles.row}>
+            <View>
+                <TouchableOpacity style={styles.element} onPress={about}>
+                    <Image style={{
+                        alignSelf: 'center',
+                        height: normalize(120),
+                        width: normalize(70),
+                    }} source={require('../../public/icons/about.png')} ></Image>
+                </TouchableOpacity>
+                <Text style={styles.text}> {'Sobre Ecofiltro'} </Text>
             </View>
             <View>
                 <TouchableOpacity style={styles.element} onPress={contact}>
                     <Image style={{
                         alignSelf: 'center',
-                        height:normalize(80),
-                        width:normalize(120),
+                        height: normalize(80),
+                        width: normalize(120),
                     }} source={require('../../public/icons/contactus.png')} ></Image>
                 </TouchableOpacity>
                 <Text style={styles.text}> {'Contáctanos'} </Text>
@@ -60,16 +61,16 @@ const Menu = ({about, contact, info}) => (
 
 export default connect(
     state => ({
-        
+
     }),
-    dispatch=>({
-        contact(){
+    dispatch => ({
+        contact() {
             Actions.Contact()
         },
-        about(){
+        about() {
             Actions.About()
         },
-        info(){
+        info() {
             Actions.Info()
         }
     }),
