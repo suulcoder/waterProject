d41951926d0942925baa377ab00107ecca469dd6
DianaXimena <dianaxime0@gmail.com>
2020-12-16 09:32:48 -0600

    settings language for ios

--- src/components/Settings/index.js
+++ src/components/Settings/index.js
@@ -1,7 +1,7 @@
 import { connect } from 'react-redux';
-import {Text, View, Picker, ScrollView, TextInput, TouchableOpacity, Switch } from 'react-native';
+import { Text, View, ScrollView, TextInput, TouchableOpacity, Picker, Platform } from 'react-native';
 import Header from '../Header';
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import styles from './styles'
 import { colors } from '../../../configuration';
 import { normalize } from '../../utils/normalize';
@@ -9,9 +9,10 @@ import * as selectors from '../../reducers'
 import * as actions from '../../actions/settings'
 import { Actions } from 'react-native-router-flux';
 import { validateEmail } from '../../utils/validate';
+import RNPickerSelect from 'react-native-picker-select';
 
-const Settings = ({name_,phone_,mail_,code_,language_,location_,automatizedAudio,submit, setLanguage, setAudio}) => {
-  
+const Settings = ({ name_, phone_, mail_, code_, language_, location_, automatizedAudio, submit, setLanguage, setAudio }) => {
+
     const [name, changeName] = useState(name_);
     const [phone, changePhone] = useState(phone_);
     const [mail, changeEmail] = useState(mail_);
@@ -31,81 +32,100 @@ const Settings = ({name_,phone_,mail_,code_,language_,location_,automatizedAudio
     };
 
     return (
-    <View style={styles.container}>
-        <Header></Header>
-        <View style={styles.content}>
-            <Text style={styles.title}>
-                {'Configuración:'}
-            </Text>
-            <ScrollView>
-                <View style={styles.row}>
-                    <Text style={styles.text}>{'Idioma: '} </Text>
-                    <Picker
-                        selectedValue={language}
-                        style={{ height: normalize(50), width: normalize(550), color:colors.black }}
-                        onValueChange={(itemValue, itemIndex) => setLanguage_(itemValue)}
-                    >
-                        <Picker.Item label="Español" value="Español" color='black' />
-                    </Picker>
-                </View>
-                <View style={styles.bar}></View>
-                <Text style={styles.text_}>{'Ajustes de usuario:'} </Text>
-                <TextInput
-                    style={styles.input}
-                    placeholder="Nombre*"
-                    value={name}
-                    placeholderTextColor="#777777" 
-                    onChangeText={text => changeName(text)}
-                    returnKeyType='next'   
-                />
-                <TextInput
-                    style={styles.input}
-                    placeholderTextColor="#777777" 
-                    keyboardType={'numeric'}x
-                    placeholder="Teléfono*"
-                    value={phone}
-                    onChangeText={text => changePhone(text)}
-                    returnKeyType='next'   
-                />
-                <TextInput
-                    style={styles.input}
-                    placeholderTextColor="#777777" 
-                    keyboardType={'email-address'}
-                    placeholder="Email"
-                    value={mail}
-                    onChangeText={text => changeEmail(text)}
-                    returnKeyType='next'   
-                />
-                {
-                    !validateEmail(mail) && mail!=='' && <Text style={styles.error}> {'ESCRIBE UN CORREO VÁLIDO'} </Text>
-                }
-                <TextInput
-                    style={styles.input}
-                    placeholderTextColor="#777777" 
-                    placeholder="Ubicación/Municipio/Departamento"
-                    value={location}
-                    onChangeText={text => changeLocation(text)}
-                    returnKeyType='next'   
-                />
-                <TextInput
-                    style={styles.input}
-                    placeholderTextColor="#777777" 
-                    placeholder="Código de ecofiltro"
-                    value={code}
-                    onChangeText={text => changeCode(text)}
-                    returnKeyType='next'   
-                />
-                <TouchableOpacity style={styles.submit} onPress={() => submit({name, phone, mail, code, location})}>
-                    <Text style={styles.submitText}> {'Guardar'} </Text>
-                </TouchableOpacity>
-            </ScrollView>
+        <View style={styles.container}>
+            <Header></Header>
+            <View style={styles.content}>
+                <Text style={styles.title}>
+                    {'Configuración:'}
+                </Text>
+                <ScrollView>
+                    <View style={styles.row}>
+                        <Text style={styles.text}>{'Idioma: '} </Text>
+                        {
+                            Platform.OS === 'ios' ?
+                                <View>
+                                    <RNPickerSelect
+                                        placeholder={{
+                                            label: 'Seleccionar idioma...',
+                                            value: null,
+                                            color: '#9EA0A4',
+                                        }}
+                                        onValueChange={(value) => console.log(value)}
+                                        items={[
+                                            { label: 'Español', value: 'Español', color: 'black' },
+                                        ]}
+                                        style={{ height: normalize(50), width: normalize(550), color: colors.black }}
+                                    />
+                                </View>
+                                :
+                                <Picker
+                                    selectedValue={language}
+                                    style={{ height: normalize(50), width: normalize(550), color: colors.black }}
+                                    onValueChange={(itemValue, itemIndex) => setLanguage_(itemValue)}
+                                >
+                                    <Picker.Item label="Español" value="Español" color='black' />
+                                </Picker>
+                        }
+                    </View>
+                    <View style={styles.bar}></View>
+                    <Text style={styles.text_}>{'Ajustes de usuario:'} </Text>
+                    <TextInput
+                        style={styles.input}
+                        placeholder="Nombre*"
+                        value={name}
+                        placeholderTextColor="#777777"
+                        onChangeText={text => changeName(text)}
+                        returnKeyType='next'
+                    />
+                    <TextInput
+                        style={styles.input}
+                        placeholderTextColor="#777777"
+                        keyboardType={'numeric'} x
+                        placeholder="Teléfono*"
+                        value={phone}
+                        onChangeText={text => changePhone(text)}
+                        returnKeyType='next'
+                    />
+                    <TextInput
+                        style={styles.input}
+                        placeholderTextColor="#777777"
+                        keyboardType={'email-address'}
+                        placeholder="Email"
+                        value={mail}
+                        onChangeText={text => changeEmail(text)}
+                        returnKeyType='next'
+                    />
+                    {
+                        !validateEmail(mail) && mail !== '' && <Text style={styles.error}> {'ESCRIBE UN CORREO VÁLIDO'} </Text>
+                    }
+                    <TextInput
+                        style={styles.input}
+                        placeholderTextColor="#777777"
+                        placeholder="Ubicación/Municipio/Departamento"
+                        value={location}
+                        onChangeText={text => changeLocation(text)}
+                        returnKeyType='next'
+                    />
+                    <TextInput
+                        style={styles.input}
+                        placeholderTextColor="#777777"
+                        placeholder="Código de ecofiltro"
+                        value={code}
+                        onChangeText={text => changeCode(text)}
+                        returnKeyType='next'
+                    />
+                    <TouchableOpacity style={styles.submit} onPress={() => submit({ name, phone, mail, code, location })}>
+                        <Text style={styles.submitText}> {'Guardar'} </Text>
+                    </TouchableOpacity>
+                </ScrollView>
+            </View>
         </View>
-    </View>
-)};
+    )
+};
 
 export default connect(
     state => ({
-        language_:selectors.getLanguage(state),
+        language_: selectors.getLanguage(state),
         name_: selectors.getName(state),
         phone_: selectors.getPhone(state),
         code_: selectors.getCode(state),
@@ -113,9 +133,9 @@ export default connect(
         mail_: selectors.getMail(state),
         automatizedAudio: selectors.getAutomatizedAudio(state)
     }),
-    dispatch=>({
-        submit({name,phone,mail,code,location}){
-            if(phone!=='' || validateEmail(mail)){
+    dispatch => ({
+        submit({ name, phone, mail, code, location }) {
+            if (phone !== '' || validateEmail(mail)) {
                 dispatch(actions.set_name(name))
                 dispatch(actions.set_code(code))
                 dispatch(actions.set_phone(phone))
@@ -124,10 +144,10 @@ export default connect(
                 Actions.pop(true)
             }
         },
-        setLanguage({language}){
-            dispatch(actions.setLanguage(language))
+        setLanguage(language) {
+            dispatch(actions.set_language(language))
         },
-        setAudio(value){
+        setAudio(value) {
             dispatch(actions.set_automatized_audio(value))
         }
     }),
