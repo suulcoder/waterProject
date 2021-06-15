30180c1502185d88856374154000f22909947926
Diana <dele18607@uvg.edu.gt>
2021-06-15 12:03:23 -0600

    About Ecofiltro

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -30,10 +30,11 @@ const About = ({ }) => {
         <View style={styles.row}>
           <Image style={{
             alignSelf: 'center',
-            height: normalize(1500),
+            height: normalize(1200),
             width: '100%',
             resizeMode: 'cover',
             borderRadius: normalize(10),
+            marginBottom: normalize(5),
           }}
             source={require('../../../assets/pics/About.jpg')}
           >
@@ -45,83 +46,93 @@ const About = ({ }) => {
         <ScrollView>
           <View style={styles.row}>
             <TouchableOpacity style={styles.element} onPress={() => Actions.HowtoUse(true)}>
-              <Text style={styles.text}> {'¿Cómo usar el Ecofiltro?'} </Text>
+              <View style={styles.row1}>
+                <Image style={{
+                  height: normalize(15),
+                  width: normalize(15),
+                  resizeMode: 'contain',
+                }}
+                  source={require('../../../assets/icons/aboutEco.png')}
+                ></Image>
+                <Text style={styles.text}> {'¿Cómo usar el Ecofiltro?'} </Text>
+              </View>
             </TouchableOpacity>
           </View>
-          { /* <View style={styles.row}>
-            <View>
-              <TouchableOpacity style={styles.element} onPress={() => Actions.HowtoUse(true)}>
+          <View style={styles.row}>
+            <TouchableOpacity style={styles.element} onPress={() => Actions.Whyitworks(true)}>
+              <View style={styles.row1}>
                 <Image style={{
-                  alignSelf: 'center',
-                  height: normalize(250),
-                  width: normalize(250),
-                  borderRadius: normalize(50),
-                }} source={require('../../../assets/img/howToUse.jpg')} ></Image>
-              </TouchableOpacity>
-              <Text style={styles.text}> {'¿Cómo usar el Ecofiltro?'} </Text>
-            </View>
-            <View>
-              <TouchableOpacity style={styles.element} onPress={() => Actions.Stories(true)}>
-                <Image style={{
-                  alignSelf: 'center',
-                  height: normalize(250),
-                  width: normalize(250),
-                  borderRadius: normalize(50),
-                }} source={require('../../../assets/img/stories_.jpeg')} ></Image>
-              </TouchableOpacity>
-              <Text style={styles.text}> {'Testimoniales'} </Text>
-            </View>
+                  height: normalize(15),
+                  width: normalize(15),
+                  resizeMode: 'contain',
+                }}
+                  source={require('../../../assets/icons/howWorks.png')}
+                ></Image>
+                <Text style={styles.text}> {'¿Cómo funciona Ecofiltro?'} </Text>
+              </View>
+            </TouchableOpacity>
           </View>
           <View style={styles.row}>
-            <View>
-              <TouchableOpacity style={styles.element} onPress={() => Linking.openURL('https://www.google.com.gt/maps/search/distribuidor+de+ecofiltro/@14.9099828,-90.7388792,8z')}>
+            <TouchableOpacity style={styles.element} onPress={() => Actions.Stories(true)}>
+              <View style={styles.row1}>
                 <Image style={{
-                  alignSelf: 'center',
-                  height: normalize(130),
-                  width: normalize(60),
+                  height: normalize(15),
+                  width: normalize(15),
+                  resizeMode: 'contain',
                 }}
-                  source={require('../../../assets/icons/mapa.png')} ></Image>
-              </TouchableOpacity>
-              <Text style={styles.text}> {'¡Encuentra el punto de venta más cercano a tí!'} </Text>
-            </View>
-            <View>
-              <TouchableOpacity style={styles.element} onPress={() => Linking.openURL('https://ecofiltro.com/es/catalogo-productos')}>
-                <Image style={{
-                  alignSelf: 'center',
-                  height: normalize(130),
-                  width: normalize(60),
-                }}
-                  source={require('../../../assets/img/productos.png')} ></Image>
-              </TouchableOpacity>
-              <Text style={styles.text}> {'Descubre los diferentes productos que ofrecemos'} </Text>
-            </View>
+                  source={require('../../../assets/icons/Testimonials.png')}
+                ></Image>
+                <Text style={styles.text}> {'Testimoniales'} </Text>
+              </View>
+            </TouchableOpacity>
           </View>
           <View style={styles.row}>
-            <View>
-              <TouchableOpacity style={styles.element} onPress={() => Actions.Whyitworks(true)}>
+            <TouchableOpacity style={styles.element} 
+              onPress={() => Linking.openURL('https://www.google.com.gt/maps/search/distribuidor+de+ecofiltro/@14.9099828,-90.7388792,8z')}
+            >
+              <View style={styles.row1}>
                 <Image style={{
-                  alignSelf: 'center',
-                  height: normalize(130),
-                  width: normalize(60),
+                  height: normalize(15),
+                  width: normalize(15),
+                  resizeMode: 'contain',
                 }}
-                  source={require('../../../assets/icons/about.png')} ></Image>
-              </TouchableOpacity>
-              <Text style={styles.text}> {'¿Cómo funciona Ecofiltro?'} </Text>
-            </View>
-            <View>
-              <TouchableOpacity style={styles.element} onPress={() => Actions.FAQ(true)}>
+                  source={require('../../../assets/icons/Ubicacion.png')}
+                ></Image>
+                <Text style={styles.text}> {'¡Encuentra el punto de venta más cercano!'} </Text>
+              </View>
+            </TouchableOpacity>
+          </View>
+          <View style={styles.row}>
+            <TouchableOpacity style={styles.element} onPress={() => Linking.openURL('https://ecofiltro.com/es/catalogo-productos')}>
+              <View style={styles.row1}>
                 <Image style={{
-                  alignSelf: 'center',
-                  height: normalize(90),
-                  width: normalize(95),
-                }} source={require('../../../assets/icons/question.png')} ></Image>
-              </TouchableOpacity>
-              <Text style={styles.text}> {'Preguntas frecuentes'} </Text>
-            </View>
-              </View> */}
+                  height: normalize(15),
+                  width: normalize(15),
+                  resizeMode: 'contain',
+                }}
+                  source={require('../../../assets/icons/Products.png')}
+                ></Image>
+                <Text style={styles.text}> {'Descubre nuestros productos'} </Text>
+              </View>
+            </TouchableOpacity>
+          </View>
+          <View style={styles.row}>
+            <TouchableOpacity style={styles.element} onPress={() => Actions.FAQ(true)}>
+              <View style={styles.row1}>
+                <Image style={{
+                  height: normalize(15),
+                  width: normalize(15),
+                  resizeMode: 'contain',
+                }}
+                  source={require('../../../assets/icons/FAQ.png')}
+                ></Image>
+                <Text style={styles.text}> {'Preguntas Frecuentes'} </Text>
+              </View>
+            </TouchableOpacity>
+          </View>
         </ScrollView>
       </View>
-      {/*<AwesomeAlert
+      <AwesomeAlert
         show={alert}
         showProgress={false}
         title="Recordatorio"
@@ -135,7 +146,7 @@ const About = ({ }) => {
         confirmButtonColor="#DD6B55"
         onCancelPressed={() => hideAlert()}
         onConfirmPressed={() => hideAlert()}
-      />*/}
+      />
     </View>
   );
 }
