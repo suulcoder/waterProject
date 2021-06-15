71925f2cf0cada0440258f07b9757e1f8a437512
Diana <dele18607@uvg.edu.gt>
2021-06-15 16:52:06 -0600

    contaminated water and health

--- src/components/Info/index.js
+++ src/components/Info/index.js
@@ -1,53 +1,66 @@
 import { connect } from 'react-redux';
-import {Text, View, Image, ScrollView, TextInput, TouchableOpacity } from 'react-native';
+import { Text, View, Image, ScrollView, TextInput, TouchableOpacity } from 'react-native';
 import Header from '../Header';
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import styles from './styles'
-import MapView, {Marker} from 'react-native-maps';
+import MapView, { Marker } from 'react-native-maps';
 import Menu from '../Menu';
 import { normalize } from '../../utils/normalize';
 import { Actions } from 'react-native-router-flux';
 
-const Info = ({}) => (
+const Info = ({ }) => (
     <View style={styles.container}>
         <Header></Header>
         <View style={styles.content}>
-            <Text style={styles.welcomeText}>
-                {'Agua contaminada y salud'}
-            </Text>
             <View style={styles.row}>
-                <View>
-                    <TouchableOpacity style={styles.element} onPress={()=>Actions.Bacterias(true)}>
-                        <Image style={{
-                            alignSelf: 'center',
-                            height:normalize(100),
-                            width:normalize(180),
-                        }} source={require('../../../assets/icons/coliform.png')} ></Image>
-                    </TouchableOpacity>
-                    <Text style={styles.text}> {'Microbios en el agua'} </Text>
-                </View>
-                <View>
-                    <TouchableOpacity style={styles.element} onPress={()=>Actions.Health(true)}>
-                        <Image style={{
-                            alignSelf: 'center',
-                            height:normalize(150),
-                            width:normalize(150),
-                        }} source={require('../../../assets/icons/health.png')} ></Image>
-                    </TouchableOpacity>
-                    <Text style={styles.text}> {'Agua y tu salud'} </Text>
+                <Image style={{
+                    alignSelf: 'center',
+                    height: normalize(1200),
+                    width: '100%',
+                    resizeMode: 'cover',
+                    borderRadius: normalize(10),
+                    marginBottom: normalize(5),
+                }}
+                    source={require('../../../assets/pics/Info.jpg')}
+                >
+                </Image>
+                <View style={{ position: 'absolute', top: 0, left: 0, width: 200, height: 300, alignItems: 'center', justifyContent: 'center' }}>
+                    <Text style={styles.welcomeText}>{'Agua\ncontaminada\ny salud'}</Text>
                 </View>
             </View>
             <View style={styles.row}>
                 <View>
-                    <TouchableOpacity style={styles.element} onPress={()=>Actions.Pollution(true)}>
+                    <TouchableOpacity style={styles.element} onPress={() => Actions.Bacterias(true)}>
                         <Image style={{
                             alignSelf: 'center',
-                            height:normalize(140),
-                            width:normalize(140),
-                            borderRadius: normalize(50),
-                        }} source={require('../../../assets/icons/pollution.png')} ></Image>
+                            height: normalize(100),
+                            width: normalize(100),
+                            resizeMode: 'contain',
+                        }} source={require('../../../assets/icons/bacterias.png')} ></Image>
                     </TouchableOpacity>
-                    <Text style={styles.text}> {'Otros contaminantes'} </Text>
+                    <Text style={styles.text}> {'Microbios\nen el agua'} </Text>
+                </View>
+                <View>
+                    <TouchableOpacity style={styles.element} onPress={() => Actions.Health(true)}>
+                        <Image style={{
+                            alignSelf: 'center',
+                            height: normalize(100),
+                            width: normalize(100),
+                            resizeMode: 'contain',
+                        }} source={require('../../../assets/icons/HealthB.png')} ></Image>
+                    </TouchableOpacity>
+                    <Text style={styles.text}> {'Agua y\ntu salud'} </Text>
+                </View>
+                <View>
+                    <TouchableOpacity style={styles.element} onPress={() => Actions.Pollution(true)}>
+                        <Image style={{
+                            alignSelf: 'center',
+                            height: normalize(100),
+                            width: normalize(100),
+                            resizeMode: 'contain',
+                        }} source={require('../../../assets/icons/PollutionB.png')} ></Image>
+                    </TouchableOpacity>
+                    <Text style={styles.text}> {'Otros\ncontaminantes'} </Text>
                 </View>
             </View>
         </View>
@@ -56,9 +69,9 @@ const Info = ({}) => (
 
 export default connect(
     state => ({
-        
+
     }),
-    dispatch=>({
-        
+    dispatch => ({
+
     }),
 )(Info);
