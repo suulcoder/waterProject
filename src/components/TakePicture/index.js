d59227938e55a17e1bd6a455298eef57143fdd49
Diana <dele18607@uvg.edu.gt>
2021-06-27 13:17:40 -0600

    camera advances

--- src/components/TakePicture/index.js
+++ src/components/TakePicture/index.js
@@ -1,19 +1,28 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { Text, View, TouchableOpacity, ImageBackground } from 'react-native';
 import { Camera } from 'expo-camera';
 import styles from './styles';
+import { Actions } from 'react-native-router-flux';
 
 const TakePicture = () => {
-  const [hasPermission, setHasPermission] = useState(null);
-  const [type, setType] = useState(Camera.Constants.Type.back);
-
+  const [hasPermission, setHasPermission] = useState(null)
+  const [previewVisible, setPreviewVisible] = useState(false)
+  const [capturedImage, setCapturedImage] = useState(null)
+  let camera;
   useEffect(() => {
-    (async () => {
-      const { status } = await Camera.requestPermissionsAsync();
-      setHasPermission(status === 'granted');
-    })();
-  }, []);
-
+    ; (async () => {
+      const { status } = await Camera.requestPermissionsAsync()
+      setHasPermission(status === 'granted')
+    })()
+  }, [])
+  const __takePicture = async () => {
+    if (!camera) return
+    const photo = await camera.takePictureAsync()
+    console.log(photo)
+    setPreviewVisible(true)
+    setCapturedImage(photo)
+  }
+  const __savePhoto = async () => { }
   if (hasPermission === null) {
     return <View />;
   }
@@ -21,22 +30,139 @@ const TakePicture = () => {
     return <Text>No access to camera</Text>;
   }
   return (
-    <View style={styles.container}>
-      <Camera style={styles.camera} type={type}>
-        <View style={styles.buttonContainer}>
-          <TouchableOpacity
-            style={styles.button}
-            onPress={() => {
-              setType(
-                type === Camera.Constants.Type.back
-                  ? Camera.Constants.Type.front
-                  : Camera.Constants.Type.back
-              );
-            }}>
-            <Text style={styles.text}> Flip </Text>
-          </TouchableOpacity>
-        </View>
-      </Camera>
+    <View
+      style={{
+        flex: 1
+      }}
+    >
+      {previewVisible ? (
+        <ImageBackground
+          source={{ uri: capturedImage && capturedImage.uri }}
+          style={{
+            flex: 1
+          }}
+        >
+          <View
+            style={{
+              flex: 1,
+              flexDirection: 'column',
+              padding: 15,
+              justifyContent: 'flex-end'
+            }}
+          >
+            <View
+              style={{
+                flexDirection: 'row',
+                justifyContent: 'space-between'
+              }}
+            >
+              <TouchableOpacity
+                onPress={() => setPreviewVisible(false)}
+                style={{
+                  width: 130,
+                  height: 40,
+
+                  alignItems: 'center',
+                  borderRadius: 4
+                }}
+              >
+                <Text
+                  style={{
+                    color: '#fff',
+                    fontSize: 20
+                  }}
+                >
+                  Re-take
+                </Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                onPress={__savePhoto}
+                style={{
+                  width: 130,
+                  height: 40,
+
+                  alignItems: 'center',
+                  borderRadius: 4
+                }}
+              >
+                <Text
+                  style={{
+                    color: '#fff',
+                    fontSize: 20
+                  }}
+                >
+                  save photo
+                </Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </ImageBackground>
+      ) : (
+        <Camera
+          style={{ flex: 1 }}
+          type={Camera.Constants.Type.back}
+          ref={(r) => {
+            camera = r
+          }}
+        >
+          <View
+            style={{
+              flex: 1,
+              backgroundColor: 'transparent',
+              flexDirection: 'row'
+            }}
+          >
+            <View
+              style={{
+                position: 'absolute',
+                top: '5%',
+                right: '5%'
+              }}
+            >
+              <TouchableOpacity onPress={() => Actions.Evaluate(false)}>
+                <Text
+                  style={{
+                    color: '#fff',
+                    fontSize: 20
+                  }}
+                >
+                  X
+                </Text>
+              </TouchableOpacity>
+            </View>
+            <View
+              style={{
+                position: 'absolute',
+                bottom: 0,
+                flexDirection: 'row',
+                flex: 1,
+                width: '100%',
+                padding: 20,
+                justifyContent: 'space-between'
+              }}
+            >
+              <View
+                style={{
+                  alignSelf: 'center',
+                  flex: 1,
+                  alignItems: 'center'
+                }}
+              >
+                <TouchableOpacity
+                  onPress={__takePicture}
+                  style={{
+                    width: 70,
+                    height: 70,
+                    bottom: 0,
+                    borderRadius: 50,
+                    backgroundColor: '#fff'
+                  }}
+                />
+              </View>
+            </View>
+          </View>
+        </Camera>
+      )}
     </View>
   );
 }
--- src/components/TakePicture/styles.js
+++ src/components/TakePicture/styles.js
@@ -5,24 +5,9 @@ import { normalize } from '../../utils/normalize';
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-    },
-    camera: {
-        flex: 1,
-    },
-    buttonContainer: {
-        flex: 1,
-        backgroundColor: 'transparent',
-        flexDirection: 'row',
-        margin: 20,
-    },
-    button: {
-        flex: 0.1,
-        alignSelf: 'flex-end',
+        backgroundColor: '#000',
         alignItems: 'center',
-    },
-    text: {
-        fontSize: 18,
-        color: 'white',
+        justifyContent: 'center'
     },
 });
 
